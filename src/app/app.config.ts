d4372ef6d3833b3cb408d4f853bb7246171588b3
Saba.Beridze <omegeboi510@gmail.com>
2024-06-08 11:22:55 +0400

    Initial commit

--- src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routerConfig } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routerConfig)]
-};
-
